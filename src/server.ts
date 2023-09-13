7a2d667fa2fefe32ee81220e26dbebcc2cf1b905
gilberto19 <gilberto19>
2023-09-13 03:26:53 -0300

    feat: apply routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import { fastify } from "fastify"
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const PORT = 3333;
 const app = fastify();
 
-app.get("/", ()=> "hello word")
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+
 app.listen({
     port: PORT
 }).then(()=> console.log(`HTTP Server running in http://localhost:${PORT}`))
